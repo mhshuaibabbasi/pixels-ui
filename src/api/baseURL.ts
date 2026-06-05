d2efb9e73e0213715c179683b309ec22f4df9f5c
mhshuaibabbasi <mhshuaibabbasi@gmail.com>
2026-06-05 14:53:16 +0530

    correct url

--- src/api/baseURL.ts
+++ src/api/baseURL.ts
@@ -1,7 +1,7 @@
 
 const isLocal = false; // Set this to true for local development
 
-export const WEB_URL = 'https://pixels-server-io0i.onrender.com/';
+export const WEB_URL = 'https://pixels-server-io0i.onrender.com/pixels/';
 export const LOCAL = 'http://localhost:3002/pixels/';
 
 export const BASE_URL = isLocal ? LOCAL : WEB_URL;
